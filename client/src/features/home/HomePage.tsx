173bd546ddbf6f3f3e077660b909e4e06339afd3
keithstruble <40516239+keithstruble@users.noreply.github.com>
2025-09-02 08:59:27 -0500

    End of section 8

--- client/src/features/home/HomePage.tsx
+++ client/src/features/home/HomePage.tsx
@@ -1,11 +1,30 @@
-import { Container, Typography } from "@mui/material";
+import { Group } from "@mui/icons-material";
+import { Box, Button,  Link,  Paper, Typography } from "@mui/material";
 
 export default function HomePage() {
   return (
-    <Container sx={{mt: 3}}>
+    <Paper sx={{ color: 'white', display: 'flex', flexDirection: 'column', gap: 6, alignItems: 'center', alignContent: 'center', justifyContent: 'center', height: '100vh',
+      backgroundImage: 'linear-gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac 89%)' }}>
       <Typography variant="h3">
         Home page
       </Typography>
-    </Container>
+
+      <Box sx={{ display: 'flex', alignItems: 'center', alignContent: 'center', color: 'white', gap: 3 }}>
+        <Group sx={{height: 110, width: 110}} />
+        <Typography variant="h1">
+          Reactivites
+        </Typography>
+      </Box>
+      <Typography variant="h2">
+        Welcome to reactivites
+      </Typography>
+
+      <Button 
+          component={Link} 
+          href="/activities"
+          size="large" variant="contained" sx={{height: 80, borderRadius: 4, fontSize: '1.5rem'}}>
+        Take me to the activities!
+      </Button>
+    </Paper>
   )
-}
+} 
